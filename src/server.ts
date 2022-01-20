0c88947768c337ce67a980507ce5462badab9abb
mohitdmak <mdmohitrc@gmail.com>
2022-01-20 22:39:54 +0530

    rev: routes, server to ts, reorganize dockerfiles, src

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,36 @@
+// ########################### Express, Redis, Error and log handlers ###########################
 // import configured express app and redis client
-const http = require("http");
-const app = require("./app").app;
-const redisClient = require("./app").redisClient;
+import { app } from "./app";
+import { redisClient } from "./app";
 
 // import logger
-const { logger } = require("./middeware/logger");
+import { logger } from "./middeware/logger";
 
 // import ERROR models and error handler
-const ERROR = require("./middeware/error_models");
-const { error_handler } = require("./middeware/error_handler");
+import * as ERROR from "./middeware/error_models";
+import { error_handler } from "./middeware/error_handler";
+// ########################### / ########################### / ###########################
+
 
 
 // ########################### Mongo database and ORM configs ###########################
 // import mongo ORM
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 //* mongoose settings for depraciation errors
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-// Importing Creds for Mongo access
-const username = require("./config/mongo.js").MONGO_INITDB_ROOT_USERNAME;
-const password = require("./config/mongo.js").MONGO_INITDB_ROOT_PASSWORD;
-
 // setting db uri
-const db_uri = `mongodb://MongoContainer:27017/Portal?authSource=admin`;
-
-// mongoose connection
-var db_connection;
+const db_uri: string = `mongodb://MongoContainer:27017/Portal?authSource=admin`;
 // ########################### / ########################### / ###########################
 
 
 
 // ########################### Connecting to Cache and db, opening port ###########################
 //* Configuring port
-let port = process.env.PORT || 3000; 
+let port: string | number = process.env.PORT || 3000; 
 
 // Open port for node app, once redis and mongodb is connected
 redisClient.on('connect', async function (){
@@ -43,28 +38,24 @@ redisClient.on('connect', async function (){
     
     try{
         //* Wait for connection to db
-        db_connection = await mongoose.connect(db_uri);
-        logger.info("Connection to Mongodb Instance established");
+        await mongoose.connect(db_uri);
+        logger.info('Connection to Mongodb Instance established');
 
         //* Opening port for express app.
         app.listen(port, () => {
             logger.info(`Server started on port: ${port} with mode: ${process.env.npm_lifecycle_event}`);
         });
+
+        // Stop for any redis errors
+        redisClient.on('error', function (err: Error) {
+            var RedisError: ERROR.DBError = new ERROR.DBError(ERROR.HttpStatusCode.INTERNAL_SERVER_ERROR, 'Could not establish a connection with redis', true);
+            RedisError.stack = err.stack;
+            throw RedisError;
+        });
     }
     catch(err){
-        if(error_handler.isHandleAble(err)){
-            await error_handler.handleError(err);
-        }
-        // Stop for any redis errors
-        redisClient.on('error', function (err) {
-            console.log('\n     Could not establish a connection with redis. \n' + err);
-        });
-        logger.error(err);
+        if(!error_handler.isHandleAble(err)) throw err; 
+        await error_handler.handleError(err)
     }
 });
 // ########################### / ########################### /###########################
-
-
-
-// export mongoose connection for configuration in app.js forest admin
-module.exports = db_connection;
